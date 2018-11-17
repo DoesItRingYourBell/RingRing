b9ea4df26a322997d5fba8e518290398c7d2c60e
Jonas Bounama <jobounama@hotmail.de>
2018-11-17 14:56:44 +0100

    added shaking and start to work on gamelogic

---
+++ DoesItRingYourBell/Assets/Scripts/GameScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour {
+  private telescript[] tele;
+	// Use this for initialization
+	void Start () {
+		tele = GetComponentsInChildren<telescript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
--- DoesItRingYourBell/Assets/Scripts/telescript.cs
+++ DoesItRingYourBell/Assets/Scripts/telescript.cs
@@ -5,13 +5,42 @@ using UnityEngine;
 public class telescript : MonoBehaviour {
   AudioSource clip;
 	// Use this for initialization
+  bool shaking = false;
+  int shaking_count = 0;
+  float stepping = 10.0f;
+  bool forward = true;
+  bool left = true;
 	void Start () {
 		clip = this.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(shaking){
+      if(shaking_count%3 == 0 && shaking_count != 0){
+        forward = !forward;
+      }
+      if(shaking_count%6 == 0 && shaking_count != 0){
+        left = !left;
+      }
+      if(forward){
+        stepping = 10.0f;
+      }else{
+        stepping = -10.0f;
+      }
+      var rot = transform.localRotation;
+      float newRot = 0;
+      if(left){
+        newRot = rot.eulerAngles.z + stepping;
+      }else{
+        newRot = rot.eulerAngles.z - stepping;
+      }
+      transform.localRotation = Quaternion.Euler(rot.eulerAngles.x, rot.eulerAngles.y, newRot);
+      shaking_count++;
+      if(shaking_count == 24){
+        shaking = false;
+      }
+    }
 	}
 
   void OnMouseDown()
@@ -19,13 +48,15 @@ public class telescript : MonoBehaviour {
     activate();
   }
 
-  void activate(){
+  public void activate(){
     Debug.Log("Hit Object: " + gameObject.name);
     clip.Play();
     shake();
   }
 
   void shake(){
-    
+    forward = true;
+    shaking_count = 0;
+    shaking = true;
   }
 }
